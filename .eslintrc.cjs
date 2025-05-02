8795b45de32d49920a337afef4acc4fe5c1b73af
pratheek28 <pratheek0928@gmail.com>
2025-05-02 09:30:12 -0700

    Testing fixes for compatibility issues between dependencies

---
+++ .eslintrc.cjs
@@ -0,0 +1,50 @@
+// .eslintrc.cjs
+
+module.exports = {
+    root: true,
+    env: {
+      browser: true,
+      es2021: true
+    },
+    extends: [
+      "next/core-web-vitals",
+      "plugin:@typescript-eslint/recommended",
+      "plugin:jsdoc/recommended",
+      "prettier"
+    ],
+    plugins: ["@typescript-eslint", "jsdoc", "jsx-a11y"],
+    rules: {
+      "jsdoc/check-alignment": "error",
+      "jsdoc/check-indentation": "warn",
+      "jsdoc/check-param-names": "error",
+      "jsdoc/require-jsdoc": [
+        "warn",
+        {
+          require: {
+            FunctionDeclaration: true,
+            MethodDefinition: true,
+            ClassDeclaration: true,
+            ArrowFunctionExpression: false,
+            FunctionExpression: false
+          }
+        }
+      ],
+      "jsx-a11y/alt-text": 1,
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          vars: "all",
+          varsIgnorePattern: "^_",
+          args: "after-used",
+          argsIgnorePattern: "^_"
+        }
+      ]
+    },
+    settings: {
+      react: {
+        version: "detect"
+      }
+    }
+  };
+  
