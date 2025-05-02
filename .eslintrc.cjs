c68332128354c74280d1990b4840f5d8c834e053
pratheek28 <pratheek0928@gmail.com>
2025-05-02 09:31:02 -0700

    Fix Prettier formatting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,50 +1,49 @@
 // .eslintrc.cjs
 
 module.exports = {
-    root: true,
-    env: {
-      browser: true,
-      es2021: true
-    },
-    extends: [
-      "next/core-web-vitals",
-      "plugin:@typescript-eslint/recommended",
-      "plugin:jsdoc/recommended",
-      "prettier"
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "next/core-web-vitals",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:jsdoc/recommended",
+    "prettier",
+  ],
+  plugins: ["@typescript-eslint", "jsdoc", "jsx-a11y"],
+  rules: {
+    "jsdoc/check-alignment": "error",
+    "jsdoc/check-indentation": "warn",
+    "jsdoc/check-param-names": "error",
+    "jsdoc/require-jsdoc": [
+      "warn",
+      {
+        require: {
+          FunctionDeclaration: true,
+          MethodDefinition: true,
+          ClassDeclaration: true,
+          ArrowFunctionExpression: false,
+          FunctionExpression: false,
+        },
+      },
     ],
-    plugins: ["@typescript-eslint", "jsdoc", "jsx-a11y"],
-    rules: {
-      "jsdoc/check-alignment": "error",
-      "jsdoc/check-indentation": "warn",
-      "jsdoc/check-param-names": "error",
-      "jsdoc/require-jsdoc": [
-        "warn",
-        {
-          require: {
-            FunctionDeclaration: true,
-            MethodDefinition: true,
-            ClassDeclaration: true,
-            ArrowFunctionExpression: false,
-            FunctionExpression: false
-          }
-        }
-      ],
-      "jsx-a11y/alt-text": 1,
-      "no-unused-vars": "off",
-      "@typescript-eslint/no-unused-vars": [
-        "error",
-        {
-          vars: "all",
-          varsIgnorePattern: "^_",
-          args: "after-used",
-          argsIgnorePattern: "^_"
-        }
-      ]
+    "jsx-a11y/alt-text": 1,
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        vars: "all",
+        varsIgnorePattern: "^_",
+        args: "after-used",
+        argsIgnorePattern: "^_",
+      },
+    ],
+  },
+  settings: {
+    react: {
+      version: "detect",
     },
-    settings: {
-      react: {
-        version: "detect"
-      }
-    }
-  };
-  
+  },
+};
